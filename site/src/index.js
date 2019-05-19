2f5b9fbfc6721d92a8031499e986c6d5be18dd63
Ryan Lee <ryanjlee@berkeley.edu>
2019-05-19 09:56:22 -0700

    Move tutorial files

--- site/src/index.js
+++ site/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CommentBox from './CommentBox';
+import CommentBox from './tutorial/CommentBox';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<CommentBox />, document.getElementById('root'));
