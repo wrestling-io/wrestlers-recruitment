a2536c827dce1dd6a1f8a34411c55e2abf80f106
Ryan Lee <ryanjlee@berkeley.edu>
2019-05-19 10:13:39 -0700

    Add wrestler model

---
+++ backend/models/wrestler.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+// create new instance of the mongoose.schema. the schema takes an
+// object that shows the shape of your database entries.
+const WrestlerSchema = new Schema({
+  first_name: String,
+  last_name: String,
+  dob: String,
+  weight: int,
+  school: String,
+}, { timestamps: true });
+
+// export our module to use in server.js
+export default mongoose.model('Wrestler', WrestlerSchema);
