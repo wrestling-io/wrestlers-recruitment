99b6bf321c48f6ba5048d16714583d8abb8feaf9
Ryan Lee <ryanjlee@berkeley.edu>
2019-05-30 12:15:21 -0700

    Route Admin page (not updated yet)

---
+++ site/src/admin/Admin.js
@@ -0,0 +1,15 @@
+// Admin.js
+import React from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
+
+const Admin = (props) => {
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  );
+};
+
+export default Admin;
--- site/src/index.js
+++ site/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import TableWrapper from './table/TableWrapper';
+import Admin from './admin/Admin';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TableWrapper />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={TableWrapper} />
+      <Route path="/admin" component={Admin} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 registerServiceWorker();
